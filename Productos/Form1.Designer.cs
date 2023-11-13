edab8241d2bf5d151d44133f423db2684aceebea
Alexis <garciaciroalexis@gmail.com>
2023-11-13 17:02:45 -0500

    proyecto productos

---
+++ Productos/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace Productos
+{
+    partial class frmClientes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvFactura = new System.Windows.Forms.DataGridView();
+            this.clmNombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clmCodigo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clmAntiguedad = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clmValor = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clmDescuento = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clmSubtotal = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clmRetencion = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clmTotal = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btmIngresar = new System.Windows.Forms.Button();
+            this.btmCalcular = new System.Windows.Forms.Button();
+            this.btmSalir = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFactura)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvFactura
+            // 
+            this.dgvFactura.AllowUserToAddRows = false;
+            this.dgvFactura.AllowUserToDeleteRows = false;
+            this.dgvFactura.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvFactura.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvFactura.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.clmNombre,
+            this.clmCodigo,
+            this.clmAntiguedad,
+            this.clmValor,
+            this.clmDescuento,
+            this.clmSubtotal,
+            this.clmRetencion,
+            this.clmTotal});
+            this.dgvFactura.Location = new System.Drawing.Point(16, 15);
+            this.dgvFactura.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dgvFactura.Name = "dgvFactura";
+            this.dgvFactura.ReadOnly = true;
+            this.dgvFactura.RowHeadersWidth = 51;
+            this.dgvFactura.Size = new System.Drawing.Size(1476, 448);
+            this.dgvFactura.TabIndex = 0;
+            this.dgvFactura.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvFactura_CellContentClick);
+            // 
+            // clmNombre
+            // 
+            this.clmNombre.HeaderText = "Nombre";
+            this.clmNombre.MinimumWidth = 6;
+            this.clmNombre.Name = "clmNombre";
+            this.clmNombre.ReadOnly = true;
+            // 
+            // clmCodigo
+            // 
+            this.clmCodigo.HeaderText = "Codigo";
+            this.clmCodigo.MinimumWidth = 6;
+            this.clmCodigo.Name = "clmCodigo";
+            this.clmCodigo.ReadOnly = true;
+            // 
+            // clmAntiguedad
+            // 
+            this.clmAntiguedad.HeaderText = "Antiguedad";
+            this.clmAntiguedad.MinimumWidth = 6;
+            this.clmAntiguedad.Name = "clmAntiguedad";
+            this.clmAntiguedad.ReadOnly = true;
+            // 
+            // clmValor
+            // 
+            this.clmValor.HeaderText = "Valor";
+            this.clmValor.MinimumWidth = 6;
+            this.clmValor.Name = "clmValor";
+            this.clmValor.ReadOnly = true;
+            // 
+            // clmDescuento
+            // 
+            this.clmDescuento.HeaderText = "Descuento";
+            this.clmDescuento.MinimumWidth = 6;
+            this.clmDescuento.Name = "clmDescuento";
+            this.clmDescuento.ReadOnly = true;
+            // 
+            // clmSubtotal
+            // 
+            this.clmSubtotal.HeaderText = "Subtotal";
+            this.clmSubtotal.MinimumWidth = 6;
+            this.clmSubtotal.Name = "clmSubtotal";
+            this.clmSubtotal.ReadOnly = true;
+            // 
+            // clmRetencion
+            // 
+            this.clmRetencion.HeaderText = "Retención";
+            this.clmRetencion.MinimumWidth = 6;
+            this.clmRetencion.Name = "clmRetencion";
+            this.clmRetencion.ReadOnly = true;
+            // 
+            // clmTotal
+            // 
+            this.clmTotal.HeaderText = "Total";
+            this.clmTotal.MinimumWidth = 6;
+            this.clmTotal.Name = "clmTotal";
+            this.clmTotal.ReadOnly = true;
+            // 
+            // btmIngresar
+            // 
+            this.btmIngresar.Location = new System.Drawing.Point(193, 516);
+            this.btmIngresar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btmIngresar.Name = "btmIngresar";
+            this.btmIngresar.Size = new System.Drawing.Size(100, 28);
+            this.btmIngresar.TabIndex = 1;
+            this.btmIngresar.Text = "Ingresar";
+            this.btmIngresar.UseVisualStyleBackColor = true;
+            this.btmIngresar.Click += new System.EventHandler(this.btmIngresar_Click);
+            // 
+            // btmCalcular
+            // 
+            this.btmCalcular.Location = new System.Drawing.Point(725, 516);
+            this.btmCalcular.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btmCalcular.Name = "btmCalcular";
+            this.btmCalcular.Size = new System.Drawing.Size(100, 28);
+            this.btmCalcular.TabIndex = 2;
+            this.btmCalcular.Text = "Calcular";
+            this.btmCalcular.UseVisualStyleBackColor = true;
+            this.btmCalcular.Click += new System.EventHandler(this.btmCalcular_Click);
+            // 
+            // btmSalir
+            // 
+            this.btmSalir.Location = new System.Drawing.Point(1296, 516);
+            this.btmSalir.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btmSalir.Name = "btmSalir";
+            this.btmSalir.Size = new System.Drawing.Size(100, 28);
+            this.btmSalir.TabIndex = 3;
+            this.btmSalir.Text = "Salir";
+            this.btmSalir.UseVisualStyleBackColor = true;
+            this.btmSalir.Click += new System.EventHandler(this.btmSalir_Click);
+            // 
+            // frmClientes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1508, 609);
+            this.ControlBox = false;
+            this.Controls.Add(this.btmSalir);
+            this.Controls.Add(this.btmCalcular);
+            this.Controls.Add(this.btmIngresar);
+            this.Controls.Add(this.dgvFactura);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "frmClientes";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Clientes";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFactura)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvFactura;
+        private System.Windows.Forms.Button btmIngresar;
+        private System.Windows.Forms.Button btmCalcular;
+        private System.Windows.Forms.Button btmSalir;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmNombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmCodigo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmAntiguedad;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmValor;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmDescuento;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmSubtotal;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmRetencion;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmTotal;
+    }
+}
+
---
+++ Productos/Form1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace Productos
+{
+    public partial class frmClientes : Form
+    {
+        public int N;
+        public int i;
+        public double[,] mInformacion;
+        public  String[] vNombres;
+
+
+        public frmClientes()
+        {
+            InitializeComponent();
+        }
+
+        private void dgvFactura_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void btmIngresar_Click(object sender, EventArgs e)
+        {
+            dgvFactura.RowCount = 0;
+            N = Convert.ToInt32(Interaction.InputBox("Ingrese el número de clientes: "));
+            vNombres = new String[N];
+            mInformacion = new double[N, 7];
+            //Agregar filas al dgv
+            dgvFactura.RowCount = (N + 4);
+            //ocultar filas que no se utilizan
+            dgvFactura.Rows[N ].Cells[0].Style.BackColor = Color.Transparent;
+            dgvFactura.Rows[N + 1].Cells[0].Style.BackColor = Color.Transparent;
+            dgvFactura.Rows[N + 2].Cells[0].Style.BackColor = Color.Transparent;
+            dgvFactura.Rows[N + 3].Cells[0].Style.BackColor = Color.Transparent;
+
+            dgvFactura.Rows[N ].Cells[1].Style.BackColor = Color.Transparent;
+            dgvFactura.Rows[N + 1].Cells[1].Style.BackColor = Color.Transparent;
+            dgvFactura.Rows[N + 2].Cells[1].Style.BackColor = Color.Transparent;
+            dgvFactura.Rows[N + 3].Cells[1].Style.BackColor = Color.Transparent;
+
+
+            dgvFactura.Rows[N ].Cells[2].Style.BackColor = Color.Transparent;
+            dgvFactura.Rows[N + 1].Cells[2].Style.BackColor = Color.Transparent;
+            dgvFactura.Rows[N + 2].Cells[2].Style.BackColor = Color.Transparent;
+            dgvFactura.Rows[N + 3].Cells[2].Style.BackColor = Color.Transparent;
+
+            //titular promedio ,mayor, menor..
+            dgvFactura.Rows[N].Cells[3].Value = "total";
+            dgvFactura.Rows[N].Cells[3].Style.BackColor = Color.SkyBlue;
+            dgvFactura.Rows[N + 1].Cells[3].Value = "promedio";
+            dgvFactura.Rows[N + 1].Cells[3].Style.BackColor = Color.SkyBlue;
+            dgvFactura.Rows[N + 2].Cells[3].Value = "Valor mayor";
+            dgvFactura.Rows[N + 2].Cells[3].Style.BackColor = Color.SkyBlue;
+            dgvFactura.Rows[N + 3].Cells[3].Value = "Valor menor";
+            dgvFactura.Rows[N + 3].Cells[3].Style.BackColor = Color.SkyBlue;
+
+
+            for (i = 0; i <= N-1; i++)
+            {   //ingresar los nombres
+                vNombres[i] = Convert.ToString(Interaction.InputBox("Ingrese el nombre del cliente: "));
+                dgvFactura.Rows[i].Cells[0].Value=vNombres[i];
+
+                mInformacion[i, 0] = Convert.ToDouble(Interaction.InputBox("ingrese el codigo de clasificación : "));
+                dgvFactura.Rows[i].Cells[1].Value = mInformacion[i, 0];
+                mInformacion[i, 1] = Convert.ToDouble(Interaction.InputBox("ingrese la antiguedad: "));
+                dgvFactura.Rows[i].Cells[2].Value = mInformacion[i, 1];
+                mInformacion[i, 2] = Convert.ToDouble(Interaction.InputBox("ingrese el valor: "));
+                dgvFactura.Rows[i].Cells[3].Value = mInformacion[i, 2];
+                //calculando descuento y subtotal en ingresar en dgv
+                if (mInformacion[i,0]==1 && mInformacion[i, 1] >= 2 && mInformacion[i,2]>20000000 )
+                {
+                    mInformacion[i, 3] = mInformacion[i, 2] * 0.253;
+                    dgvFactura.Rows[i].Cells[4].Value = mInformacion[i, 3];
+                    mInformacion[i, 4] = mInformacion[i, 2] - mInformacion[i, 3];
+                    dgvFactura.Rows[i].Cells[5].Value = mInformacion[i, 4];
+                }else if (mInformacion[i, 0] == 2 && mInformacion[i, 1] >= 5 && mInformacion[i, 2] > 30000000)
+                {
+                    mInformacion[i, 3] = mInformacion[i, 2] * 0.18;
+                    dgvFactura.Rows[i].Cells[4].Value = mInformacion[i, 3];
+                    mInformacion[i, 4] = mInformacion[i, 2] - mInformacion[i, 3];
+                    dgvFactura.Rows[i].Cells[5].Value = mInformacion[i, 4];
+                }
+                else if (mInformacion[i, 0] == 3  && mInformacion[i, 2] >= 17500000)
+                {
+                    mInformacion[i, 3] = mInformacion[i, 2] * 0.1;
+                    dgvFactura.Rows[i].Cells[4].Value = mInformacion[i, 3];
+                    mInformacion[i, 4] = mInformacion[i, 2] - mInformacion[i, 3];
+                    dgvFactura.Rows[i].Cells[5].Value = mInformacion[i, 4];
+                }
+                else
+                {
+                    mInformacion[i, 3] = 0;
+                    dgvFactura.Rows[i].Cells[4].Value = mInformacion[i, 3];
+                    mInformacion[i, 4] = mInformacion[i, 2] - mInformacion[i, 3];
+                    dgvFactura.Rows[i].Cells[5].Value = mInformacion[i, 4];
+                }
+                //calcular la retencion de fuente
+                if (mInformacion[i, 4] > 30000000) 
+                {
+                    mInformacion[i, 5] = mInformacion[i, 4] * 0.028;
+                    dgvFactura.Rows[i].Cells[6].Value = mInformacion[i, 5];
+                }
+                else if (mInformacion[i,4]>10000000)
+                {
+                    mInformacion[i, 5] = mInformacion[i, 4] * 0.017;
+                    dgvFactura.Rows[i].Cells[6].Value = mInformacion[i, 5];
+                }
+                else if(mInformacion[i,4]<10000000)
+                {
+                    mInformacion[i, 5] = mInformacion[i, 4] * 0.0045;
+                    dgvFactura.Rows[i].Cells[6].Value = mInformacion[i, 5];
+                }
+                //calcular el total a pagar
+                mInformacion[i, 6] = mInformacion[i, 4] + mInformacion[i, 5];
+                dgvFactura.Rows[i].Cells[7].Value = mInformacion[i, 6];
+
+
+
+            }
+            btmCalcular.Enabled = true;
+            btmIngresar.Enabled = false;
+
+        }
+
+        private void btmCalcular_Click(object sender, EventArgs e)
+        {
+            double[,] vtotales = new double[4, 4];
+            int c;
+            double acum = 0, vMayor = 0, vMenor = 0;
+
+            for (c=3;c<=6; c++)
+            {
+                for (i = 0; i <= N - 1; i++)
+                {
+                    acum = acum + mInformacion[i, c];
+                    if (i==0)
+                    {
+                        vMayor = mInformacion[i,c ];
+                        vMenor = mInformacion[i, c];
+                    }
+                    else 
+                    {
+                        if (mInformacion[i,c]>vMayor)
+                        {
+                            vMayor = mInformacion[i, c];
+                        }
+                        else
+                        {
+                            if (mInformacion[i,c]<vMenor)
+                            {
+                                vMenor = mInformacion[i, c];
+                            }
+                        }
+                    }
+                }
+                vtotales[0, c - 3] = acum;
+                dgvFactura.Rows[N].Cells[c + 1].Value = vtotales[0, c - 3];
+                vtotales[1, c - 3] = acum / N;
+                dgvFactura.Rows[N + 1].Cells[c + 1].Value = vtotales[1, c - 3];
+                vtotales[2, c - 3] = vMayor;
+                dgvFactura.Rows[N + 2].Cells[c + 1].Value = vtotales[2, c - 3];
+                vtotales[3, c - 3] = vMenor;
+                dgvFactura.Rows[N + 3].Cells[c + 1].Value = vtotales[3, c - 3];
+                acum = 0;
+                btmIngresar.Enabled = true;
+                btmCalcular.Enabled = false;
+            }
+
+        }
+
+        private void btmSalir_Click(object sender, EventArgs e)
+        {
+            DialogResult Salir= MessageBox.Show("¿está seguro de salir de la aplicación?", "Salir", MessageBoxButtons.YesNo);
+            if (Salir == DialogResult.Yes) Close();
+        }
+    }
+}
